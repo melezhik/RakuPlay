2739b9d16aaac51d3fd6ccf3989f0b44f0640c44
Alexey Melezhik <melezhik@gmail.com>
2020-12-18 03:49:31 +0000

    better example

--- issues/2920/task.bash
+++ issues/2920/task.bash
@@ -1,7 +1,11 @@
-set -x
+#set -x
+
 set -e
 raku --version
 
-raku -e "say '>>>'; { (2, 4 ... 1_000_000).gist; say (now - ENTER now).Int }; say '<<<'"
+cat $root_dir/task.bash
 
+echo '>>>'
+raku -e "{ (2, 4 ... 1_000_000).gist; say (now - ENTER now).Int }"
+echo '<<<'
 
