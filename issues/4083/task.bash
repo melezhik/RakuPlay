7c2af8f6f5cd4c47225adf4d726db9aee67b80ea
Alexey Melezhik <melezhik@gmail.com>
2020-12-18 18:17:17 -0600

    refactoring

--- issues/4083/task.bash
+++ issues/4083/task.bash
@@ -1,2 +1,3 @@
-set -x
+cat $root_dir/task.bash
+
 raku -e 'sub foo($x is rw) { }; foo(1)' 2>&1
--- issues/4119/task.bash
+++ issues/4119/task.bash
@@ -1,5 +1,7 @@
 set -x
 set -e
 
+raku --version
+
 raku -e 'sub infix:<ab>($a, $b) is assoc<chain> { True }; say 1 !ab 2';
 
